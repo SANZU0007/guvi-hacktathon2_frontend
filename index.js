29ed5db8c0a25d4bae34f234d7a894adef371e5b
SANZU@DESKTOP-85PAAKK.(none) <SANZU@DESKTOP-85PAAKK.(none)>
2023-07-13 21:26:15 +0530

    backend + nodejs

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const http = require("http");
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "content-type": "text/plain" });
-  response.end("hello world");
-});
+const express = require('express')
+const app = express();
+let notes = [
+    {
+        id:1,
+        content:'html is easy',
+        important: true
+    },
+    {
+        id:2,
+        content:'backend applications using notejs',
+        important: false
+    },
+    {
+        id:3,
+        content:'get and post method important oh http',
+        important: true
+    }
+];
+
+app.get('/',((request,response)=>{
+    response.send('<h1>Notes app</h1>')
+
+}
+))
+app.get('/api/notes',((request,response)=>{
+    response.json(notes);
+}))
+
+
+
+
 
 const PORT = 3001;
 app.listen(PORT);
